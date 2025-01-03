d9fdaaec149d8468de80680f40e170587cc322f6
andseipa <andseipa@gmail.com>
2025-01-03 18:36:06 +0100

    fix: rearranging alt key in karabiner and ghostty

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,7 +8,7 @@ keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.del("n", "<leader><tab>[")
 keymap.del("n", "<leader><tab>]")
-keymap.del("n", "<leader>wm")
+-- keymap.del("n", "<leader>wm")
 
 keymap.set("n", "<leader><tab>h", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 keymap.set("n", "<leader><tab>l", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
@@ -16,8 +16,8 @@ keymap.set("n", "<leader><tab>l", "<cmd>tabprevious<cr>", { desc = "Previous Tab
 keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
-keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- Move lines or selection up/down using Command + Arrow Keys
 -- Unmap existing bindings for Up and Down arrow keys
@@ -26,8 +26,9 @@ keymap.set("n", "<A-Down>", "<cmd>m .+1<CR>==", { desc = "Move line down with Co
 keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down with Command-Down" })
 keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up with Command-Up" })
 
-keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+keymap.del("n", "<leader>|")
+keymap.del("n", "<leader>-")
+keymap.set("n", '<leader>"', "<C-W>s", { desc = "Split Window Below", remap = true })
 keymap.set("n", "<leader>%", "<C-W>v", { desc = "Split Window Right", remap = true })
 
--- Snacks.toggle.zoom():map("<leader>wf"):map("<leader>uZ"):unmap("<leader>wm")
 Snacks.toggle.zoom():map("<leader>wf"):map("<leader>uZ")
